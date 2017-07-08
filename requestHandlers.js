77513bf6226200acfc16deacdc6e47e0961b91b3
Ryota <ryt.0081@gmail.com>
2017-07-08 22:46:31 +0900

    create html file to be read

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,23 +5,25 @@ var querystring = require("querystring"),
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" '+
-    'content="text/html; charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" enctype="multipart/form-data" '+
-    'method="post">'+
-    '<input type="file" name="upload" multiple="multiple">'+
-    '<input type="submit" value="Upload file" />'+
-    '</form>'+
-    '</body>'+
-    '</html>';
+  // var body = '<html>'+
+  //   '<head>'+
+  //   '<meta http-equiv="Content-Type" '+
+  //   'content="text/html; charset=UTF-8" />'+
+  //   '</head>'+
+  //   '<body>'+
+  //   '<form action="/upload" enctype="multipart/form-data" '+
+  //   'method="post">'+
+  //   '<input type="file" name="upload" multiple="multiple">'+
+  //   '<input type="submit" value="Upload file" />'+
+  //   '</form>'+
+  //   '</body>'+
+  //   '</html>';
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
+    fs.readFile(__dirname + "/index.html", function(error, data){
+      response.writeHead(200, {"Content-Type": "text"});
+      response.write(data);
+      response.end();
+    });
 }
 
 function upload(response, request) {
