c7a055895bba890142d4103220a95b7a1de4c829
Ryota <ryt.0081@gmail.com>
2017-07-12 23:08:24 +0900

    file divided

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,21 +4,6 @@ var querystring = require("querystring"),
 
 function start(response) {
   console.log("Request handler 'start' was called.");
-
-  // var body = '<html>'+
-  //   '<head>'+
-  //   '<meta http-equiv="Content-Type" '+
-  //   'content="text/html; charset=UTF-8" />'+
-  //   '</head>'+
-  //   '<body>'+
-  //   '<form action="/upload" enctype="multipart/form-data" '+
-  //   'method="post">'+
-  //   '<input type="file" name="upload" multiple="multiple">'+
-  //   '<input type="submit" value="Upload file" />'+
-  //   '</form>'+
-  //   '</body>'+
-  //   '</html>';
-
     fs.readFile(__dirname + "/index.html", function(error, data){
       response.writeHead(200, {"Content-Type": "text"});
       response.write(data);
